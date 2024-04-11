dd7295d626cacc29279ce2421718e892d588fea7
kcwww <cwkim0321@gmail.com>
2024-04-11 23:32:42 +0900

    feat: redirect home

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/navigation';
+
 const Login = () => {
+  const router = useRouter();
+
+  router.replace('/home'); // Redirect to home page logged in after
+
   return <main>Login</main>;
 };
 
