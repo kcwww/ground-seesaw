cf67e66ce91f9348f66159dae3ccb8f6ba81d2a9
kcwww <cwkim0321@gmail.com>
2024-04-11 15:19:07 +0900

    feat: add ROUTES CONSTANT

--- app/(protected)/_components/Sidebar/NavBar.tsx
+++ app/(protected)/_components/Sidebar/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {
   Home,
   Newspaper,
@@ -5,34 +6,35 @@ import {
   CalendarHeart,
   MailQuestion,
 } from 'lucide-react';
-import Link from 'next/link';
+
+import { ROUTES } from '@/constants/routes';
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link className="flex gap-2" href="/home">
+          <Link className="flex gap-2" href={ROUTES.HOME}>
             <Home /> Home
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2" href="/threads">
+          <Link className="flex gap-2" href={ROUTES.THREADS}>
             <Newspaper /> Threads
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2" href="/events">
+          <Link className="flex gap-2" href={ROUTES.EVENTS}>
             <CalendarHeart /> Events
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2" href="/contact">
+          <Link className="flex gap-2" href={ROUTES.CONTACT}>
             <MailQuestion /> Contact
           </Link>
         </li>
         <li>
-          <Link className="flex gap-2" href="/post">
+          <Link className="flex gap-2" href={ROUTES.POST}>
             <SquarePen /> Post
           </Link>
         </li>
---
+++ constants/routes.ts
@@ -0,0 +1,13 @@
+const ROUTES = {
+  /* landing layout */
+  ROOT: '/',
+  HOME: '/home',
+  THREADS: '/threads',
+  EVENTS: '/events',
+  CONTACT: '/contact',
+  POST: '/post',
+} as const;
+
+const BACKEND_ROUTES = {} as const;
+
+export { BACKEND_ROUTES, ROUTES };
