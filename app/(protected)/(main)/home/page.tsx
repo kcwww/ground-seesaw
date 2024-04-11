5e612948dcc9500a70545fca711343f997723268
kcwww <cwkim0321@gmail.com>
2024-04-11 16:14:23 +0900

    fix: response data

--- app/(protected)/(main)/home/page.tsx
+++ app/(protected)/(main)/home/page.tsx
@@ -1,13 +1,23 @@
 import { BACKEND_ORIGIN } from '@/constants/url';
 
 const fetchData = async () => {
-  const response = await fetch(`${BACKEND_ORIGIN}/api/recent-posts`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(`${BACKEND_ORIGIN}/api/recent-posts`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
 const HomePage = async () => {
   const data = await fetchData();
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className="md:ml-[20rem]">
       <h1>Home Page</h1>
