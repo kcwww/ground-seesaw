11f2e5855436a7d16150d9cd4d1cff2a9435040b
kcwww <cwkim0321@gmail.com>
2024-04-12 00:03:08 +0900

    feat: add Header navigating

--- app/(protected)/_components/Sidebar/SideBarHeader.tsx
+++ app/(protected)/_components/Sidebar/SideBarHeader.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import SideBarIcon from '@/public/imgs/icons/logo.png';
+import { ROUTES } from '@/constants/routes';
 
 const SideBarHeader = () => {
   return (
     <header>
-      <div className="flex gap-2 justify-start items-center">
-        <Image src={SideBarIcon} alt="logo" width={50} height={50} />
-        <div className="header__title">GROUND SEESAW</div>
-      </div>
+      <Link href={ROUTES.HOME}>
+        <div className="flex gap-2 justify-start items-center">
+          <Image src={SideBarIcon} alt="logo" width={50} height={50} />
+          <div className="header__title">GROUND SEESAW</div>
+        </div>
+      </Link>
     </header>
   );
 };
