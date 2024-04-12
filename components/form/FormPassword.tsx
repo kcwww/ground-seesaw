0d8c031b5f41fb6eef41873719c8ed7135f279f9
kcwww <cwkim0321@gmail.com>
2024-04-12 16:38:30 +0900

    feat: add FormPassword

---
+++ components/form/FormPassword.tsx
@@ -0,0 +1,33 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import type { FormFieldType } from '@/lib/types/formfield';
+
+const FormPassword = ({ form, isLoading }: FormFieldType) => {
+  return (
+    <FormField
+      control={form.control}
+      name="password"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel htmlFor={field.name}>Password</FormLabel>
+          <FormControl>
+            <Input
+              disabled={isLoading}
+              placeholder="비밀번호를 입력해주세요."
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormPassword;
