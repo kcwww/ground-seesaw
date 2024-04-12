2c2e2a009fdf38fe92e84a0a3e264792739b2c0b
kcwww <cwkim0321@gmail.com>
2024-04-12 16:38:04 +0900

    feat: add FormImages

---
+++ components/form/FormImages.tsx
@@ -0,0 +1,37 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import type { FormFieldType } from '@/lib/types/formfield';
+
+const FormImages = ({ form, isLoading }: FormFieldType) => {
+  return (
+    <>
+      <FormField
+        control={form.control}
+        name="images"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel htmlFor={field.name}>Images</FormLabel>
+            <FormControl>
+              <Input
+                disabled={isLoading}
+                type="file"
+                multiple={true}
+                {...field}
+                value={undefined}
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </>
+  );
+};
+
+export default FormImages;
