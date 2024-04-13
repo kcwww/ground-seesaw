cc5004fdc592903dd95be0dbd6d4ca760298efdd
kcwww <cwkim0321@gmail.com>
2024-04-13 14:57:28 +0900

    feat: add submit Modal POST

--- components/modal/SubmitModal.tsx
+++ components/modal/SubmitModal.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import {
   AlertDialog,
@@ -12,23 +18,60 @@ import {
 } from '@/components/ui/alert-dialog';
 import { useModal } from '@/lib/hooks/useModal';
 import s3Upload from '@/lib/s3/s3upload';
+import { BACKEND_ROUTES, ROUTES } from '@/constants/routes';
+import clientComponentFetch from '@/lib/fetch/clientFetch';
 
 const SubmitModal = () => {
   const { onClose, isOpen, type, props } = useModal();
+  const [isUploading, setIsUploading] = useState(false);
+  const router = useRouter();
 
   if (type !== 'Submit') return null;
-  console.log(props);
 
   const postData = async () => {
     const { images, description, nickname, password } = props.data as any;
-    console.log(images, description, nickname, password);
+    setIsUploading(true);
+
     try {
       const { uploadImg } = s3Upload();
       const imgUrls = await uploadImg(images.map((img: any) => img.file));
-      console.log(imgUrls);
+      const date = new Date();
+      date.setHours(date.getHours() + 9);
+      const koreaDate = date.toISOString().slice(0, 19).replace('T', ' ');
+
+      const data = {
+        Images: imgUrls,
+        author: nickname,
+        date: koreaDate,
+        description,
+        location: '',
+        password,
+        views: 0,
+        likes: 0,
+        comments: null,
+      };
+
+      try {
+        await clientComponentFetch(BACKEND_ROUTES.THREAD, {
+          method: 'POST',
+          body: JSON.stringify(data),
+        });
+
+        toast.success('포스팅이 완료되었습니다.');
+        router.push(ROUTES.HOME);
+        router.refresh();
+      } catch (error) {
+        console.error('Error posting data', error);
+        toast.error('포스팅에 실패했습니다.');
+        router.push('/404');
+        router.refresh();
+      }
     } catch (error) {
       console.error('Error uploading image', error);
+      toast.error('이미지 업로드에 실패했습니다.');
+      router.push('/404');
     }
+    setIsUploading(false);
   };
 
   return (
@@ -46,6 +89,7 @@ const SubmitModal = () => {
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel
+            disabled={isUploading}
             onClick={() => {
               onClose();
             }}
@@ -53,9 +97,10 @@ const SubmitModal = () => {
             닫기
           </AlertDialogCancel>
           <AlertDialogAction
+            disabled={isUploading}
             onClick={async () => {
-              onClose();
               await postData();
+              onClose();
             }}
           >
             포스팅
