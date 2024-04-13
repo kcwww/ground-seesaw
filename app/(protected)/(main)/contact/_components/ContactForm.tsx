2e43966eee6e7779d557a3cbe97ee470d6594e68
kcwww <cwkim0321@gmail.com>
2024-04-13 19:04:29 +0900

    refactor: seperate contact Form field

--- app/(protected)/(main)/contact/_components/ContactForm.tsx
+++ app/(protected)/(main)/contact/_components/ContactForm.tsx
@@ -6,19 +6,12 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
+import { Form } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
-import { Textarea } from '@/components/ui/textarea';
-import { Input } from '@/components/ui/input';
 import clientComponentFetch from '@/lib/fetch/clientFetch';
 import { BACKEND_ROUTES } from '@/constants/routes';
+
+import ContactFormField from '@/app/(protected)/(main)/contact/_components/ContactFormField';
 
 export const ContactFormSchema = z.object({
   subject: z.string().min(1, {
@@ -74,65 +67,7 @@ const ContactForm = () => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="w=full flex flex-col gap-8"
         >
-          <FormField
-            control={form.control}
-            name="subject"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel htmlFor={field.name}>제목</FormLabel>
-                <FormControl>
-                  <Input
-                    id={field.name}
-                    disabled={form.formState.isSubmitting}
-                    placeholder="제목을 입력해주세요."
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel htmlFor={field.name}>이메일</FormLabel>
-                <FormControl>
-                  <Input
-                    id={field.name}
-                    type="email"
-                    disabled={form.formState.isSubmitting}
-                    placeholder="답장이 필요하시다면 이메일을 입력해주세요."
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <div className="flex justify-between px-2">
-                  <FormLabel htmlFor={field.name}>메세지</FormLabel>
-                  {form.watch('message').length + ' / 1000'}
-                </div>
-                <FormControl>
-                  <Textarea
-                    id={field.name}
-                    className="h-32"
-                    disabled={form.formState.isSubmitting}
-                    placeholder="내용을 입력해주세요."
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <ContactFormField form={form} />
           <Button
             type="submit"
             disabled={form.formState.isSubmitting || loading}
