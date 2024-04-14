54774d6bb518ba5b5c5332bb7d0e19ab7a68c1bf
kcwww <cwkim0321@gmail.com>
2024-04-15 05:52:42 +0900

    fix: notification error

--- lib/fetch/fetchPostData.ts
+++ lib/fetch/fetchPostData.ts
@@ -2,11 +2,20 @@ import clientComponentFetch from '@/lib/fetch/clientFetch';
 import { BACKEND_ROUTES } from '@/constants/routes';
 import type { PostType } from '@/lib/types/postType';
 
+import serverComponentFetch from './serverFetch';
+
 const fetchPostData = async (postId: string, type: string) => {
   try {
+    if (type === 'notifications') {
+      const res = await serverComponentFetch(
+        BACKEND_ROUTES.POST_DETAIL(postId) + '?type=' + type
+      );
+      return res.data as PostType;
+    }
     const res = await clientComponentFetch(
       BACKEND_ROUTES.POST_DETAIL(postId) + '?type=' + type
     );
+
     return res.data as PostType;
   } catch (error) {
     console.error(error);
