a49080ce502060d8e19209c0271f84a7c2dbe67e
kcwww <cwkim0321@gmail.com>
2024-04-18 14:23:27 +0900

    feat : add event lists api

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { collection, addDoc } from 'firebase/firestore';
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
 
 import { GroundSeeSawDB } from '@/Firebase';
 
@@ -18,4 +24,43 @@ const POST = async (req: NextRequest) => {
   }
 };
 
-export { POST };
+const GET = async (req: NextRequest) => {
+  try {
+    const docRef = collection(GroundSeeSawDB, 'events');
+    const snapshot = await getDocs(docRef);
+
+    // if snapshot's createAt time is 1 days ago, delete the document
+    // snapshot's createAt format is 2012-12-12 12:12:12
+    const date = new Date();
+    date.setUTCHours(date.getUTCHours() + 9);
+    const koreaDate = date.toISOString().slice(0, 10);
+
+    const result = snapshot.docs.map((eventDoc) => {
+      const data = eventDoc.data();
+      const docDate = data.createAt.slice(0, 10);
+      if (docDate < koreaDate) {
+        const docRef = doc(GroundSeeSawDB, 'events', eventDoc.id);
+        await deleteDoc(docRef);
+        return null;
+      }
+
+      return {
+        id: eventDoc.id,
+        ...eventDoc.data(),
+      };
+    });
+
+    const events = result.filter((event) => event !== null);
+
+    return NextResponse.json({
+      message: 'Recent events retrieved successfully',
+      data: events,
+    });
+  } catch (e) {
+    return NextResponse.json({
+      message: 'Error retrieving documents: ' + e,
+    });
+  }
+};
+
+export { POST, GET };
